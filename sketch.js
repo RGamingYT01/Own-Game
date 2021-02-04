a15b98f6e0d405bbaf55eb68003ef6925360ddc4
RGamingYT01 <72249361+RGamingYT01@users.noreply.github.com>
2021-02-04 13:15:59 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,58 @@
-var zombie = [];
+
+var zomb1,zomb2,zomb3;
 var shooter;
-var shooterImg,zomb1,zomb2;
+var shooterImg;
+var bgImg;
+var zombImg1,zombImg2,zombImg3;
 
 function preload(){
-shooterImg = loadImage("sprites/shooter.png")
+shooterImg = loadImage("sprites/shooter.png");
+bgImg = loadImage("sprites/ground.jpg");
+zombImg1 = loadImage("sprites/zomb1.png");
+zombImg2 = loadImage("sprites/zomb2.png");
+zombImg3 = loadImage("sprites/zomb3.png");
 }
 function setup(){
-createCanvas(400,400)
+createCanvas(displayWidth,displayHeight)
 shooter = createSprite(200,200)
 shooter.addImage(shooterImg)
+shooter.scale = 0.5;
+
 }
 function draw(){
 
-background("lightGreen")
+background(bgImg);
+spawnObstacles();
 drawSprites();
+camera.x = shooter.x;
+camera.y = shooter.y;
 }
+function spawnObstacles() {
+    if(frameCount % 30 === 0) {
+      var zomb = createSprite(random(0,displayWidth-40),random(-200,displayHeight),200,200);
+      zomb.velocityX = -4;
+      
+      //generate random obstacles
+      var rand = Math.round(random(1,3));
+      switch(rand) {
+        case 1:
+        zomb.addImage(zombImg1);
+                break;
+        case 2: 
+        zomb.addImage(zombImg2);
+                break;
+        case 3:
+        zomb.addImage(zombImg3);
+                break;
+ 
+        default: break;
+      }
+      
+      //assign scale and lifetime to the obstacle           
+      zomb.scale = 0.8;
+      zomb.lifetime = 600;
+      //add each obstacle to the group
+     // zomb.add(obstacle);
+     
+    }
+  }
